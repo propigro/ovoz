48ef83c33d82e82cc546be794065db4927b6dafd
Lazy Programmer <propigro@gmail.com>
2024-11-28 18:02:57 +0500

    vesion 1.0

---
+++ main.py
@@ -0,0 +1,14 @@
+from gtts import gTTS
+import os
+
+# Matnni belgilash
+text = ""
+
+# Ovoz yaratish (gTTS - Google Text-to-Speech)
+language = 'en'  # Changed to English ('en')
+tts = gTTS(text=text, lang=language, slow=False)
+
+# Ovoz faylini saqlash
+audio_file = "output.mp3"
+tts.save(audio_file)
+
